f6206c4da45d4729f42af51b27471e570cc1deba
rexsimiloluwah <adetoyosi2001@gmail.com>
2023-02-11 12:56:15 +0100

    modified the index page title

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Hello World' });
+  res.render('index', { title: 'Test GH Actions Deploy' });
 });
 
 module.exports = router;
